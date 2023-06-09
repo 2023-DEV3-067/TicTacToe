c3ca8226b61c2315d38cbdf31cc5dc77009c3490
2023-DEV3-067 <cvanaerschot@positivethinking.tech>
2023-06-09 17:12:55 +0200

    ADD calculateWinner + matchnull

--- src/components/UI/Game.js
+++ src/components/UI/Game.js
@@ -3,22 +3,35 @@ import React, { useState } from 'react';
 import Board from './Board';
 
 export default function Game() {
-
-  const [gameHistory, setGameHistory] = useState([Array(9).fill(null)]);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-  const currentSquares = gameHistory[currentMove];
 
-const handlePlay = (nextSquares) => {
-  const nextHistory = [...gameHistory.slice(0, currentMove + 1), nextSquares];
-  setGameHistory(nextHistory);
-  setCurrentMove(nextHistory.length - 1);
-};
 
-return (
+
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  const handlePlay = (nextSquares) => {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  };
+
+
+
+
+
+const restartGame = (e) => {
+    e.preventDefault();
+    setHistory([Array(9).fill(null)]);
+    setCurrentMove(0);
+}
+  return (
     <div className="game">
       <div className="game-board">
-        <Board squares={currentSquares} onPlay={handlePlay} />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
+      <button onClick={(e) => restartGame(e)} className='btn'>Restart</button>
     </div>
   );
 }
